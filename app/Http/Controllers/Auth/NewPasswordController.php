df4d8d105d51c8ec3218ca67403bd1330fc2f144
TanvirHossainKhondaker <tanvirhossainkhondaker.info@gmail.com>
2023-09-12 22:08:31 +0600

    complete auth

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -19,7 +19,7 @@ class NewPasswordController extends Controller
      */
     public function create(Request $request): View
     {
-        return view('auth.reset-password', ['request' => $request]);
+        return view('auth_new.passwords.reset', ['request' => $request]);
     }
 
     /**
