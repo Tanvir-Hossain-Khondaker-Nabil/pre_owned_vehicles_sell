c9b91c4d8a858da61de6bb83a6a6f547d5a4ddef
Hridoy Roy <41943565+hridoy-roy@users.noreply.github.com>
2023-09-15 14:14:13 +0600

    Change Supplier And Remove Unnecessary Template

--- resources/views/supplier/list.blade.php
+++ resources/views/supplier/list.blade.php
@@ -8,106 +8,64 @@
 @slot('li_1') Dashboard @endslot
 @slot('title') Supplier List @endslot
 @endcomponent
-    {{-- <div class="page-content"> --}}
-        <div class="container-fluid">
-
-            <!-- start page title -->
-            {{-- <div class="row">
-                <div class="col-12">
-                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Editable Table</h4>
-
-                        <div class="page-title-right">
-                            <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                                <li class="breadcrumb-item active">Editable Table</li>
-                            </ol>
-                        </div>
-
-                    </div>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Supplier Table</h4>
+                <div class="table-responsive">
+                    <table class="table table-editable table-nowrap align-middle table-edits">
+                        <thead>
+                            <tr>
+                                <th>#SL</th>
+                                <th>Name</th>
+                                <th>Image</th>
+                                <th>Driving License No</th>
+                                <th>Email</th>
+                                <th>NID</th>
+                                <th>Phone One</th>
+                                <th>Phone Two</th>
+                                <th>Address</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($suppliers as $supplier)
+                            <tr data-id="{{++$loop->index}}">
+                                <td>{{++$loop->index}}</td>
+                                <td class="text-body fw-bold">{{$supplier->name}}</td>
+                                <td><img src="{{$supplier->avatar}}" class="rounded-circle avatar-md" alt="Profile Pic">
+                                </td>
+                                <td>{{$supplier->driving_license_no}}</td>
+                                <td>{{$supplier->email}}</td>
+                                <td>{{$supplier->nid}}</td>
+                                <td>{{$supplier->phone_1}}</td>
+                                <td>{{$supplier->phone_2}}</td>
+                                <td>{{$supplier->address}}</td>
+                                <td>
+                                    <div class="d-flex gap-3">
+                                        <a class="text-success" href="{{route('suppliers.edit',$supplier->id)}}"
+                                            title="Edit">
+                                            <i class="mdi mdi-pencil font-size-18"></i>
+                                        </a>
+                                        <form method="post" id="{{'form_'.$supplier->id}}"
+                                            action="{{route('suppliers.destroy',$supplier->id)}}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn p-0 text-danger"
+                                                data-id="{{$supplier->id}}"><i
+                                                    class="mdi mdi-delete font-size-18"></i></button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-            </div> --}}
-            <!-- end page title -->
 
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-
-                            <h4 class="card-title">Supplier Table</h4>
-                            <div class="table-responsive">
-                                <table class="table table-editable table-nowrap align-middle table-edits">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Image</th>
-                                            <th>Name</th>
-                                            <th>Driving License No</th>
-                                            <th>Email</th>
-                                            <th>NID</th>
-                                            <th>Phone One</th>
-                                            <th>Phone Two</th>
-                                            <th>Address</th>
-                                            <th>Edit</th>
-                                            <th>Delete</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @php
-                                        $sl=1
-                                        @endphp
-
-                                        @foreach($suppliers as $supplier)
-                                        <tr data-id="{{$sl++}}">
-                                            <td data-field="id" style="width: 80px">{{$sl++}}</td>
-                                            <td data-field="avatar" style="width: 80px"><img src="{{$supplier->avatar}}" class="img-fluid" alt=""></td>
-                                            <td data-field="name">{{$supplier->name}}</td>
-                                            <td data-field="driving_license_no">{{$supplier->driving_license_no}}</td>
-                                            <td data-field="email">{{$supplier->email}}</td>
-                                            <td data-field="nid">{{$supplier->nid}}</td>
-                                            <td data-field="phone_1">{{$supplier->phone_1}}</td>
-                                            <td data-field="phone_2">{{$supplier->phone_2}}</td>
-                                            <td data-field="address" >{{$supplier->address}}</td>
-                                            <td style="width: 100px">
-                                                <a class="btn btn-outline-secondary btn-sm edit"  href="{{route('suppliers.edit',$supplier->id)}}" title="Edit">
-                                                    <i class="fas fa-pencil-alt"></i>
-                                                </a>
-                                            </td>
-                                            <td>
-                                                <form method="post" id="{{'form_'.$supplier->id}}" action="{{route('suppliers.destroy',$supplier->id)}}">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-outline-secondary btn-sm delete" data-id="{{$supplier->id}}"><i class="fa-solid fa-trash"></i></button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                    </table>
-                            </div>
-
-                        </div>
-                    </div>
-                </div> <!-- end col -->
-            </div> <!-- end row -->
-
-        </div> <!-- container-fluid -->
-    {{-- </div> --}}
-    <!-- End Page-content -->
-
-    
-    <footer class="footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-6">
-                    <script>document.write(new Date().getFullYear())</script> © Skote.
-                </div>
-                <div class="col-sm-6">
-                    <div class="text-sm-end d-none d-sm-block">
-                        Design & Develop by Themesbrand
-                    </div>
-                </div>
             </div>
         </div>
-    </footer>
+    </div> <!-- end col -->
+</div>
 @endsection
